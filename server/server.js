8098b304dd17ac8306652926a9d75adcd1559aee
Victor Ampuero <vampuero@paperlessla.com>
2017-10-03 18:51:38 -0300

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'seba@example.com',
+    text: 'Hey. What is going on',
+    createdAt: 123
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect', (socket) => {
     console.log('User was disconnected');
   });
